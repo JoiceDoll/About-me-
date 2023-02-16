d9e91b447e8c6d6ecd1b3e4566f7b62f30c13abd
Joice Doll <99621429+JoiceDoll@users.noreply.github.com>
2023-02-15 21:07:45 -0300

    terceira versao

---
+++ controller.js
@@ -0,0 +1,14 @@
+// const body = document.querySelector("body");
+
+// function mudaTema() {
+//   const mudaTema = document.querySelector("#mudaTema");
+//   mudaTema.addEventListener("click", (e) => {
+//     if ((body.style.background = "#222220")) {
+//       body.style.background = "#fff";
+//     } else {
+//       body.style.background = "#222220";
+//     }
+//   });
+// }
+
+// mudaTema();
